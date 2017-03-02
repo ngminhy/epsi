3f49aca1313f34a826fe3e8de0f58cbd354f948c
xuanlv0703 <levanxuan@gmail.com>
2017-03-02 09:53:46 +0700

    Long add filter customAuthorizeAttribute for check role in admin

--- webapp/epsi/epsi/App_Start/FilterConfig.cs
+++ webapp/epsi/epsi/App_Start/FilterConfig.cs
@@ -8,6 +8,24 @@ namespace epsi
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new CustomAuthorizeAttribute());
+        }
+        public class CustomAuthorizeAttribute : AuthorizeAttribute
+        {
+            protected override bool AuthorizeCore(HttpContextBase httpContext)
+            {
+                var routeData = httpContext.Request.RequestContext.RouteData;
+                var area = routeData.DataTokens["area"];
+                var user = httpContext.User;
+               if (area != null && area.ToString() == "Admin")
+                {
+                    if (!user.Identity.IsAuthenticated)
+                        return false;
+                    if (!user.IsInRole("admin"))
+                        return false;
+                }
+                return true;
+            }
         }
     }
 }
