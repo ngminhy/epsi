259f893bcc0418e96fa2eb5f73b48e62927f920a
longpham <longpn2008@gmail.com>
2017-03-04 10:10:27 +0700

    load data to slider

--- webapp/epsi/epsi/Controllers/HomeController.cs
+++ webapp/epsi/epsi/Controllers/HomeController.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using epsi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using epsi.ViewModels;
 
 namespace epsi.Controllers
 {
     public class HomeController : Controller
     {
+        Biz4Db db = new Biz4Db();
         public ActionResult Index()
         {
             return View();
@@ -25,6 +28,22 @@ namespace epsi.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Slider()
+        {
+            var banners = db.Banners.Where(p => p.Tag == "Top").OrderBy(p => p.BannerId).ToList();
+            return PartialView("_slider", banners);
+        }
+
+        public ActionResult TopMenu()
+        {
+            var menus = db.Menus.Where(p => p.Tag == "Top").OrderBy(p => p.Order).ToList();
+            //foreach (var item in menus)
+            //{
+            //    item.SubMenus = db.Menus.Where(p => p.ParentId == item.MenuId && p.Tag == "Top").OrderByDescending(p => p.Order).ThenBy(p => p.MenuId).Select(p => new LinkDto() { Title = p.Text, Link = p.Link, MenuId = p.MenuId }).ToList();
+            //}
+            return PartialView("_menu", menus);
         }
     }
 }
