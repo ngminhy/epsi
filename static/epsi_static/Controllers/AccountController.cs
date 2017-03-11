ebeb2c0dc6f1cd190dce6e1a50c6fa7270af4bb9
Hieu Huynh <huynhngoclehieu@gmail.com>
2017-03-11 12:32:11 +0700

    login, forgot, reset

---
+++ static/epsi_static/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace epsi_static.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Forgottenpassword()
+        {
+            return View();
+        }
+        public ActionResult Resetpassword()
+        {
+            return View();
+        }
+    }
+}
