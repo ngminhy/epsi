32a00382e7b5b39367514cffcfa4239f4e2668fe
Hieu Huynh <huynhngoclehieu@gmail.com>
2017-03-12 18:58:27 +0700

    cart page

---
+++ static/epsi_static/Controllers/ShopingCartController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace epsi_static.Controllers
+{
+    public class ShopingCartController : Controller
+    {
+        // GET: ShopingCart
+        public ActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
