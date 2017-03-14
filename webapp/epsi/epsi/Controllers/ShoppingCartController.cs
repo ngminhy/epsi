fed9785363da3c809197347b07c2cae5dbeb8cb6
xuanlv0703 <levanxuan@gmail.com>
2017-03-14 20:51:48 +0700

    shopping cart

--- webapp/epsi/epsi/Controllers/ShoppingCartController.cs
+++ webapp/epsi/epsi/Controllers/ShoppingCartController.cs
@@ -1,4 +1,6 @@
-﻿using epsi.ViewModels;
+﻿using epsi.Helper;
+using epsi.Models;
+using epsi.ViewModels;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -10,6 +12,7 @@ namespace epsi.Controllers
 {
     public class ShoppingCartController : Controller
     {
+        Biz4Db db = new Biz4Db();
         // GET: ShoppingCart
         public ActionResult Index()
         {
@@ -29,15 +32,57 @@ namespace epsi.Controllers
             {
 
                 //check not exist account then save account
+
+
                 //save to order and orderdetail
+                var order = new Order();
+                order.FullName = info.FullName;
+                order.Email = info.Email;
+                order.Address = info.Address;
+                order.Phone = info.Phone;
+                order.Note = info.Note;
+                order.Total = 0;
+                order.PaymentType = info.PaymentType;
+                order.OrderStatusId = 1;
+                order.OrderDate = DateTime.Now;
+                order.OrderCode = UtilHelper.GetUniqueKey(8);
+                //Save Order
+                db.Orders.Add(order);
+                db.SaveChanges();
+
+
+                decimal orderTotal = 0;
+                // adding the order details for each
+                foreach (var item in cart)
+                {
+                    var productitem = db.Products.Where(p => p.ProductId == item.id).FirstOrDefault();
+                    var productprice = item.price;
+                    if (productitem != null)
+                    {
+                        productprice = productitem.Price;
+                    }
+                    var orderDetail = new OrderDetail
+                    {
+                        ProductId = item.id,
+                        OrderId = order.OrderId,
+                        UnitPrice = productprice,
+                        Quantity = item.count
+                    };
+                    // Set the order total of the shopping cart
+                    orderTotal += (item.count * productprice);
+
+                    db.OrderDetails.Add(orderDetail);
+
+                }
+                order.Total = orderTotal;
+                db.SaveChanges();
 
                 //send mail to customer
                 //strBody = strBody + "<h2>Thông tin người mua:</h2>Họ và Tên:  " + order.FullName + " <br />Số điện thoại: " + order.Phone + " <br />Email: " + order.Email + " <br />Địa chỉ: " + order.Address + " <br />Thanh toán: " + order.PaymentType + " (Chuyển khoản | Tiền mặt) <br />Ghi chú: " + order.Note + " <br /> <br />";
                 //strBody = strBody + "<h3>Đơn hàng số: " + order.OrderId.ToString() + "</h3>";
                 //SendEmail(order.Email, strBody);
-                //return RedirectToAction("Complete",
-                //    new { id = order.OrderId });
-                return Json(new { id = 1 });
+              
+                return Json(new { id = order.OrderCode });
 
             }
             catch
--- webapp/epsi/epsi/ViewModels/InfoOrder.cs
+++ webapp/epsi/epsi/ViewModels/InfoOrder.cs
@@ -9,5 +9,9 @@ namespace epsi.ViewModels
     {
         public string FullName { get; set; }
         public string Email { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Note { get; set; }
+        public int PaymentType { get; set; }
     }
 }
