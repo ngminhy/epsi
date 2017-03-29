c2210dc8d4885485444d72e0024a0ffdb8adef0f
MinhY.Nguyen <ngminhy@gmail.com>
2017-03-29 23:28:14 +0700

    update product model for search

--- webapp/epsi/epsi/Models/Product.cs
+++ webapp/epsi/epsi/Models/Product.cs
@@ -11,19 +11,73 @@ namespace epsi.Models
     public class Product
     {
         public int ProductId { get; set; }
-        [StringLength(200)]
-        public string Name { get; set; }
-        [StringLength(200)]
-        public string Code { get; set; }
 
+        private string name;
+        [StringLength(200)]
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                this.name = value;
+                this.UpdateSearchField();
+            }
+        }
+
+        private string code;
+        [StringLength(200)]
+        public string Code
+        {
+            get
+            {
+                return this.code;
+            }
+
+            set
+            {
+                this.code = value;
+                this.UpdateSearchField();
+            }
+        }
+
+        private string desc;
         [StringLength(1000)]
-        public string Description { get; set; }
-        
+        public string Description
+        {
+            get
+            {
+                return this.desc;
+            }
+
+            set
+            {
+                this.desc = value;
+                this.UpdateSearchField();
+            }
+        }
+
         [AllowHtml]
         public String Content { get; set; }
 
+        private string tags;
         [StringLength(200)]
-        public string Tags { get; set; }
+        public string Tags
+        {
+            get
+            {
+                return this.tags;
+            }
+
+            set
+            {
+                this.tags = value;
+                this.UpdateSearchField();
+            }
+        }
         public bool Active { get; set; }
         public bool IsSpecial { get; set; }
         public bool IsNew { get; set; }
@@ -33,17 +87,98 @@ namespace epsi.Models
         public int BasePrice { get; set; }
         public int Discount { get; set; }
         public int Price { get; set; }
-        public string Colors { get; set; }
-        public string Material { get; set; }
-        public string Power { get; set; }
-        public string Size { get; set; }
+        private string colors;
+        public string Colors
+        {
+            get
+            {
+                return this.colors;
+            }
+
+            set
+            {
+                this.colors = value;
+                this.UpdateSearchField();
+            }
+        }
+        private string material;
+        public string Material
+        {
+            get
+            {
+                return this.material;
+            }
+
+            set
+            {
+                this.material = value;
+                this.UpdateSearchField();
+            }
+        }
+
+        private string power;
+        public string Power
+        {
+            get
+            {
+                return this.power;
+            }
+
+            set
+            {
+                this.power = value;
+                this.UpdateSearchField();
+            }
+        }
+
+        private string size;
+        public string Size
+        {
+            get
+            {
+                return this.size;
+            }
+
+            set
+            {
+                this.size = value;
+                this.UpdateSearchField();
+            }
+        }
         public string OtherFeatures { get; set; }
         [StringLength(200)]
         public string MainImage { get; set; }
 
-        public int CategoryId { get; set; }
+        private int categoryId;
+        public int CategoryId
+        {
+            get
+            {
+                return this.categoryId;
+            }
+
+            set
+            {
+                this.categoryId = value;
+                this.UpdateSearchField();
+            }
+        }
         public int ParentId { get; set; }
-        public string PageURL { get; set; }
+
+        private string pageURL;
+        public string PageURL
+        {
+            get
+            {
+                return this.pageURL;
+            }
+
+            set
+            {
+                this.pageURL = value;
+                this.UpdateSearchField();
+            }
+        }
 
         public Product() { }
         public Product(ProductDto model)
@@ -71,5 +206,18 @@ namespace epsi.Models
             CreatedDate = DateTime.Now;
             PageURL = model.PageURL;
         }
+
+        private void UpdateSearchField()
+        {
+            this.Search = string.Format("{0} {1} {2} {3} {4} {5} {6} {7} {8}", this.Code, this.Colors, this.Description, this.Material, this.Name, this.Power, this.PageURL, (this.CategoryId > 0) ? string.Empty : this.GetCategory(this.CategoryId), this.Size);
+        }
+        private string GetCategory(int categoryId)
+        {
+            //Biz4Db db = new Biz4Db();
+            //var cate = db.Categorys.SingleOrDefault(p => p.CategoryId == categoryId);
+            //return (cate == null) ? string.Empty : cate.Name + cate.PageURL + cate.Description; 
+            return categoryId + " den chieu sang philip";
+        }
+        public string Search { get; private set; }
     }
 }
