236fedd372d508715aa206bb952d462cc60654b7
Hieu Huynh <huynhngoclehieu@gmail.com>
2017-04-13 00:09:51 +0700

    admin static page

---
+++ admin_static/admin_static/Controllers/GuideController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace admin_static.Controllers
+{
+    public class GuideController : Controller
+    {
+        // GET: Guide
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Form()
+        {
+            return View();
+        }
+        public ActionResult Input()
+        {
+            return View();
+        }
+    }
+}
---
+++ admin_static/admin_static/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="admin_static.MvcApplication" Language="C#" %>
